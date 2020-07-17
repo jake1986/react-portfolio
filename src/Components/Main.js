9250e3de2d4fd8af62e0db18baf824c91078820f
Jake Rhoads <jake.rhoads@gmail.com>
2020-07-16 22:19:20 -0400

    changed About page path to default

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -8,7 +8,7 @@ import { Switch, Route } from 'react-router-dom';
 
 const Main = () => (
     < Switch >
-        <Route exact path="/aboutme" component={About} />
+        <Route exact path="/" component={About} />
         <Route exact path="/projects" component={Projects} />
         <Route exact path="/resume" component={Resume} />
         <Route exact path="/contact" component={Contact} />
